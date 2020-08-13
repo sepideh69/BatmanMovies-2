20f1c51bed07f026e2333e8ad4587849e1b06ca4
sepideh69 <sepid_ak@yahoo.com>
2020-08-13 17:11:27 +0430

    movies list and detail done

---
+++ app/src/main/java/com/example/thebatman/domain/MovieCache.kt
@@ -0,0 +1,17 @@
+package com.example.thebatman.domain
+
+import com.example.thebatman.domain.entities.MovieEntity
+import com.example.thebatman.domain.entities.Container
+import io.reactivex.Observable
+
+interface MoviesCache {
+
+    fun clear()
+    fun save(movieEntity: MovieEntity)
+    fun remove(movieEntity: MovieEntity)
+    fun saveAll(movieEntities: List<MovieEntity>)
+    fun getAll(): Observable<List<MovieEntity>>
+    fun get(movieId: String): Observable<Container<MovieEntity>>
+    fun search(query: String): Observable<List<MovieEntity>>
+    fun isEmpty(): Observable<Boolean>
+}
